03f93a94010d3da9192fc43e7a2837638251e5f5
miketown77 <74637036+miketown77@users.noreply.github.com>
2023-12-03 00:53:50 -0500

    Added Caching to prevent re-loading on each chat

--- meetinggpt_website.py
+++ meetinggpt_website.py
@@ -16,8 +16,31 @@ openai.api_key = st.secrets['OPEN_API_KEY']
 if "response" not in st.session_state:
     st.session_state["response"] = "Incomplete"
 
-# def change_user_input_state():
-#     st.session_state["input"] = "done"
+if "processing" not in st.session_state:
+    st.session_state["processing"] = "Not Ready"
+
+def change_user_input_state():
+    st.session_state["response"] = "Incomplete"
+    st.session_state["processing"] = "Ready"
+
+@st.cache_data
+def user_input_cache(input):
+    return input
+
+@st.cache_data
+def first_api_call(transcript):
+    input_tokens, output_tokens, answer, response_time = gpt35.gpt35_meeting_analytics(raw_text)
+    return input_tokens, output_tokens, answer, response_time
+
+@st.cache_data
+def second_api_call(transcript):
+    reformat_input_tokens, reformat_output_tokens, reformat_answer, reformat_response_time = gpt35.reformat_gpt35(answer)
+    return reformat_input_tokens, reformat_output_tokens, reformat_answer, reformat_response_time
+
+@st.cache_data
+def reformat_df(df_in):
+    df, main_topics_df, engagement_df = post_process_output.reformat_for_visuals(df_in)
+    return df, main_topics_df, engagement_df
 
 st.set_page_config(page_title="MeetingGPT", page_icon=":tada", layout="wide")
 
@@ -35,30 +58,36 @@ st.markdown("""
          
          """)
 
-user_input = st.file_uploader(" ### Upload a Meeting Transcript or Meeting Video File for Processing:", # on_change=change_user_input_state,
+
+user_input = st.file_uploader(" ### Upload a Meeting Transcript or Meeting Video File for Processing:", on_change=change_user_input_state,
                               type=['txt', 'mp3', 'mp4', 'wav', 'avi', 'mov', 'mkv', 'flv', 'wmv'])
 
-st.markdown("# MeetingGPT Processing")
-if user_input is not None:
-    file_details = {"filename":user_input.name,
-            "filetype":user_input.type,
-            "filesize":user_input.size}
-    # st.write(file_details)
-    if user_input.type == "text/plain":
-        progress_bar = st.progress(0)
-        raw_text = str(user_input.read(), "utf-8")
-        input_tokens, output_tokens, answer, response_time = gpt35.gpt35_meeting_analytics(raw_text)
-        progress_bar.progress(25)
-        reformat_input_tokens, reformat_output_tokens, reformat_answer, reformat_response_time = gpt35.reformat_gpt35(answer)
-        progress_bar.progress(50)
-        df = pd.DataFrame({"meeting_id": [user_input.name], "final_response": [reformat_answer]})
-        df, main_topics_df, engagement_df = post_process_output.reformat_for_visuals(df)
-        progress_bar.progress(75)
-        fig = meeting_analytic_visuals.generate_website_visual(df, main_topics_df, engagement_df)
-        progress_bar.progress(100)
-        st.plotly_chart(fig)
-        st.session_state['response'] = "Complete"
+if st.session_state["response"] == "Incomplete":
+    st.session_state['messages'] = []
 
+if st.session_state["processing"] == "Ready":
+    st.markdown("# MeetingGPT Processing")
+    if user_input is not None:
+        file_details = {"filename":user_input.name,
+                "filetype":user_input.type,
+                "filesize":user_input.size}
+        # st.write(file_details)
+        if user_input.type == "text/plain":
+            progress_bar = st.progress(0)
+            raw_text = str(user_input.read(), "utf-8")
+            file_name = user_input.name
+            raw_text = user_input_cache(raw_text)
+            input_tokens, output_tokens, answer, response_time = first_api_call(raw_text)
+            progress_bar.progress(25)
+            reformat_input_tokens, reformat_output_tokens, reformat_answer, reformat_response_time = second_api_call(answer)
+            progress_bar.progress(50)
+            df = pd.DataFrame({"meeting_id": [file_name], "final_response": [reformat_answer]})
+            df, main_topics_df, engagement_df = reformat_df(df)
+            progress_bar.progress(75)
+            fig = meeting_analytic_visuals.generate_website_visual(df,main_topics_df,engagement_df)
+            progress_bar.progress(100)
+            st.plotly_chart(fig)
+            st.session_state['response'] = "Complete"
 
 if st.session_state['response'] == "Complete":
     st.title("ChatBot for " + str(user_input.name))
@@ -69,7 +98,7 @@ if st.session_state['response'] == "Complete":
     for message in st.session_state.messages:
         with st.chat_message(message.get("role")):
             st.markdown(message.get("content"))
-
+    
     prompt = st.chat_input("Ask a question about the Meeting")
     if prompt:
         with st.chat_message("user"):
