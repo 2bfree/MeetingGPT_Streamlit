21693a5081d628bc6848dea01b3eaaf4477fc541
Siva Chamiraju <schamiraju@berkeley.edu>
2023-12-03 14:45:13 -0800

    aws transcribe

---
+++ meetinggpt_website_withaws.py
@@ -0,0 +1,205 @@
+import streamlit as st
+import time
+import boto3
+import openai
+import pandas as pd
+import json
+import uuid
+import os
+import gpt35
+import post_process_output
+import meeting_analytic_visuals
+import plotly
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+import requests
+
+
+os.environ["AWS_ACCESS_KEY_ID"] = st.secrets['AWS_ACCESS_KEY_ID']
+os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets['AWS_SECRET_ACCESS_KEY']
+os.environ["AWS_DEFAULT_REGION"] = st.secrets['AWS_DEFAULT_REGION']
+
+
+
+# AWS Transcribe Functions
+def upload_to_s3(uploaded_file, bucket_name, object_name=None):
+    s3_client = boto3.client('s3')
+    if object_name is None:
+        object_name = uploaded_file.name
+
+    s3_client.upload_fileobj(uploaded_file, bucket_name, object_name)
+    return f"s3://{bucket_name}/{object_name}"
+
+
+# Function for processing transcribe result with speaker identification
+def convert_to_text(data):
+    text_content = ""
+    current_speaker = None
+    sentence = ""
+
+    for item in data['results']['items']:
+        if item['type'] == 'pronunciation':
+            speaker_label = item['speaker_label'] if 'speaker_label' in item else "Unknown"
+            # Handle speaker change
+            if current_speaker is not None and speaker_label != current_speaker:
+                text_content += f"\n{current_speaker}: {sentence.strip()}"
+                sentence = ""
+            current_speaker = speaker_label
+            sentence += item['alternatives'][0]['content'] + " "
+        elif item['type'] == 'punctuation':
+            sentence += item['alternatives'][0]['content']
+            if item['alternatives'][0]['content'] in [".", "?", "!"]:
+                text_content += f"\n{current_speaker}: {sentence.strip()}"
+                sentence = ""
+
+    if sentence.strip():
+        text_content += f"\n{current_speaker}: {sentence.strip()}"
+
+    return text_content
+
+def download_and_extract_transcript(transcript_uri):
+    # Use the requests module to get the content of the URL
+    response = requests.get(transcript_uri)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        transcript_json = response.json()
+        transcript = transcript_json
+        return transcript
+    else:
+        raise Exception(f"Failed to download transcript: {response.status_code}")
+
+
+def transcribe(uploaded_file, st_progress_bar, bucket_name):
+    # Initialize the Boto3 AWS Transcribe client
+    transcribe_client = boto3.client('transcribe')
+
+    # Create a unique name for the transcription job
+    job_name = "transcription_" + str(uuid.uuid4())
+
+    st_progress_bar.progress(2)
+    s3_uri = upload_to_s3(uploaded_file, bucket_name)
+    st_progress_bar.progress(5)
+
+    # Start the transcription job
+    transcribe_client.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': s3_uri},
+        MediaFormat=uploaded_file.type.split('/')[1],
+        LanguageCode='en-US',
+        Settings={
+                'ShowSpeakerLabels': True,
+                'MaxSpeakerLabels': 5,
+            }
+    )
+
+    # Update progress bar while waiting for the job to complete
+    for _ in range(10, 100, 10):
+        st_progress_bar.progress(_)
+        time.sleep(10)
+        status = transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+
+    transcript_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    transcript = download_and_extract_transcript(transcript_uri)
+    return transcript
+
+# Streamlit Interface
+openai.api_key = st.secrets['OPEN_API_KEY']
+bucket_name = st.secrets['AWS_BUCKET_NAME']  
+
+# Initialize session_state variables
+if 'response' not in st.session_state:
+    st.session_state['response'] = "Incomplete"
+
+if 'processing' not in st.session_state:
+    st.session_state['processing'] = "Not Ready"
+
+st.set_page_config(page_title="MeetingGPT", page_icon=":tada", layout="wide")
+
+st.markdown(" # Meeting GPT - Meeting Analytics App")
+st.markdown(" #### UCB MIDS Capstone Team: Siva Chamiraju, Francis Lee, Chase Madson, Michael Townsend")
+st.markdown("""
+         _MeetingGPT_ is a product designed to take in a meeting transcript
+         or video file and generate the following features:
+
+         1. **Abstract Summary** - Succinct summary of the uploaded meeting
+         2. **Action Items** - List of action items identified in the meeting & who they were assigned to
+         3. **Key Topics** - Bar chart displaying the main topics discussed in order of % of time discussed
+         4. **Speaking Time** - Pie Chart displaying distribution of speaking time across participants
+         5. **Sentiment** - One-word sentiment classification for meeting labeled by color as Positive (Green), Neutral (Gray) or Negative (Red)
+         """)
+
+user_input = st.file_uploader("Upload a Meeting Transcript or Meeting Video File for Processing:", type=['txt', 'mp3', 'mp4', 'wav', 'avi', 'mov', 'mkv', 'flv', 'wmv'])
+
+if user_input is not None:
+    progress_bar = st.progress(0)
+    raw_text = None
+
+    # Handle audio/video files
+    if user_input.type in ['audio/mpeg', 'video/mp4', 'audio/wav', 'video/avi', 'video/mov', 'video/mkv', 'video/flv', 'video/wmv']:
+        transcribe_text = transcribe(user_input, progress_bar, bucket_name)
+        # Debugging: Print the raw transcription text
+        #print("Raw Transcription Text:", transcribe_text)
+        try:
+            #transcribe_json_text = json.loads(transcribe_text)
+            raw_text = convert_to_text(transcribe_text)
+        except json.JSONDecodeError as e:
+            st.error(f"Error parsing transcription result: {e}")
+            print("Error parsing transcription result:", e)
+            raw_text = ""  # or handle the error appropriately
+
+        # Handle text files
+    elif user_input.type == "text/plain":
+        raw_text = str(user_input.read(), "utf-8")
+
+    if raw_text:
+        #print("Raw Transcription Text:", raw_text)
+        # Process the text transcript
+        file_name = user_input.name
+        progress_bar.progress(25)
+        input_tokens, output_tokens, answer, response_time = gpt35.gpt35_meeting_analytics(raw_text)
+        progress_bar.progress(50)
+        reformat_input_tokens, reformat_output_tokens, reformat_answer, reformat_response_time = gpt35.reformat_gpt35(answer)
+        progress_bar.progress(75)
+        df = pd.DataFrame({"meeting_id": [file_name], "final_response": [reformat_answer]})
+        df, main_topics_df, engagement_df = post_process_output.reformat_for_visuals(df)
+        fig = meeting_analytic_visuals.generate_website_visual(df, main_topics_df, engagement_df)
+        progress_bar.progress(100)
+        st.plotly_chart(fig)
+        st.session_state['response'] = "Complete"
+
+
+if st.session_state['response'] == "Complete":
+    st.title("ChatBot for " + str(user_input.name))
+
+    if "messages" not in st.session_state:
+        st.session_state['messages'] = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message.get("role")):
+            st.markdown(message.get("content"))
+    
+    prompt = st.chat_input("Ask a question about the Meeting")
+    if prompt:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            full_response = ""
+            input_query = gpt35.qa_extraction_gpt35_input_query(prompt, raw_text)
+            for response in openai.ChatCompletion.create(
+                                model="gpt-3.5-turbo-16k",
+                                temperature=0,
+                                messages=input_query,
+                                stream=True
+                                ):
+                full_response += response.choices[0].delta.get("content", "")
+                message_placeholder.markdown(full_response + "| ")
+            message_placeholder.markdown(full_response)
+        
+        st.session_state.messages.append({"role": "assistant", "content": full_response}) 
